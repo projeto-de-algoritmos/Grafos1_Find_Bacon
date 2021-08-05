ab1457d3c978c7a268b121e0f048b2d1313207e8
joaovitorrms <joaovytor0@gmail.com>
2021-08-04 22:31:50 -0300

    Add recursive function to build kevin bacon graph
    Co-authored-by: jpmartins201 <jpmartins201@live.com>

--- wikipedia.py
+++ wikipedia.py
@@ -4,7 +4,6 @@ import time
 
 
 WIKIPEDIA_API_URL = "https://en.wikipedia.org/w/api.php"
-COUNT = 0
 
 def get_page_links(page_title):
     """
@@ -32,40 +31,35 @@ def get_page_links(page_title):
     if int(page_id) != -1:
         page_links_list = page_data["query"]["pages"][page_id]["links"]
         filtered_pages = [page_link["title"] for page_link in page_links_list]
-        return page_title, filtered_pages
+        return filtered_pages
     else:
-        return page_title, []
+        return []
 
-def recursive_consume_list(lista, graph, title):
-    if lista:
-        page_title, pages_list = get_page_links(title)
-        graph[page_title] = pages_list
-        last_elem = lista.pop()
-        return recursive_consume_list(lista, graph, last_elem)
-    else:
-        return lista
-
-def build_graph(page_titles, graph):
+def recursive_consume_list(page_titles, graph, pages_set, count=0):
     for title in page_titles:
-        page_title, pages_list = get_page_links(title)
-        if page_title not in graph:
-            graph[page_title] = pages_list
-        else:
-            graph[page_title].append(pages_list)
-        lista = pages_list.copy()
-        for page in pages_list:
-            recursive_consume_list(pages_list, graph, page)
-        graph[title] = lista
-        break
-    return graph
+        count+=1
+        new_page_titles = get_page_links(title)
+        graph[title] = new_page_titles
+        pages_set.update(new_page_titles)
+
+        if count < 6:
+            recursive_consume_list(new_page_titles, graph, pages_set, count)
 
 if __name__ == "__main__":
-    _, kevin_bacon_page_links = get_page_links("Kevin Bacon")
-    kevin_bacon_graph = {
+    kevin_bacon_page_links = get_page_links("Kevin Bacon")
+    graph = {
         "Kevin Bacon": kevin_bacon_page_links
     }
+    pages_set = set()
 
-    graph = build_graph(kevin_bacon_page_links, kevin_bacon_graph)
+    recursive_consume_list(kevin_bacon_page_links, graph, pages_set)
 
+    graph_existing_keys = set(graph.keys())
+    keys_not_present_in_graph = pages_set.difference(graph)
+    nodes_without_connection = dict.fromkeys(keys_not_present_in_graph, [])
+    graph.update(nodes_without_connection)
+
+    print(f'graph: {graph}\n')
+    
     pretty_print = pprint.PrettyPrinter()
     pretty_print.pprint(graph)
