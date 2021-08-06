87a98f1889630de4933c36a37ed4bed0148ff49c
jpmartins201 <jpmartins201@live.com>
2021-08-06 20:44:02 -0300

    Adding dfs() search for end node
    Co-authored-by: joaovitor3 <joaovytor0@gmail.com>

--- find_kevin_bacon/kevin_degree/graph.py
+++ find_kevin_bacon/kevin_degree/graph.py
@@ -1,4 +1,9 @@
 class KevinBaconGraph(object):
+    
+    # def __init__(self, visited):
+    #     self.visited = set() # Set to keep track of visited nodes.
+
+
     def build_graph_from_wikipedia_page(self, page_titles, graph, pages_set, wikipedia_client, count=0):
         for title in page_titles:
             count+=1
@@ -15,3 +20,44 @@ class KevinBaconGraph(object):
         keys_not_present_in_graph = pages_set.difference(graph)
         nodes_without_connection = dict.fromkeys(keys_not_present_in_graph, [])
         graph.update(nodes_without_connection)
+
+    def dfs(self, graph, start, end, path=[], visited=[]):
+        if start in visited:
+            return path
+        path += [start]
+        visited += [start]
+
+        if start == end:
+            return path
+        for edge in graph[start]:
+            if edge not in visited:
+                return self.dfs(graph,edge,end,path,visited)
+
+    # def dfs_find_node(self, graph, end, visited, path):
+    #     start = 'Kevin Bacon'
+    #     visited[start] = 1
+    #     path.append(start)
+    #     print(f"start node {start}")
+
+    #     if start == end:
+    #         print(f"this is the path {path}")
+    #         return path
+    #     else:
+    #         print(f"stack {path}")
+    #         for node in graph[start]:
+    #             print(f" in node - {node}")
+    #             if visited[node]== -1 and node != -1 and node != 0 :
+    #                 print(f" calling for next recursive funtion {node} ")
+    #                 l = dfs_find_node(graph,node,end,visited,path)
+    #                 if l is not None:
+    #                     return path
+    #         po =  path.pop()
+    #         print(f" poped last {po}")
+    #         visited[start] = -1
+    
+    # def dfs(self, visited, graph, node):
+    #     if node not in visited:
+    #         print (node)
+    #         visited.add(node)
+    #         for neighbour in graph[node]:
+    #             dfs(visited, graph, neighbour)
--- find_kevin_bacon/kevin_degree/management/commands/build_graph.py
+++ find_kevin_bacon/kevin_degree/management/commands/build_graph.py
@@ -1,6 +1,7 @@
 from kevin_degree.client import WikipediaClient
 from kevin_degree.graph import KevinBaconGraph
 from django.core.management.base import BaseCommand
+from kevin_degree.models import Graph
 
 class Command(BaseCommand):
     help = 'Build graph from wikipedia page links using Kevin Bacon as starting node'
@@ -21,6 +22,9 @@ class Command(BaseCommand):
         kevin_bacon_graph = KevinBaconGraph()
         kevin_bacon_graph.build_graph_from_wikipedia_page(kevin_bacon_page_links, graph, pages_set, wikipedia_client)
         kevin_bacon_graph.add_nodes_without_connections_to_graph(graph, pages_set)
-        print(graph)
+        path = kevin_bacon_graph.dfs(graph,'Kevin Bacon', "(I'm Gonna) Love Me Again")
+        # db_graph = Graph.objects.create(json_graph=graph)
+        print(path)
+        # print(graph)
 
-
+    
