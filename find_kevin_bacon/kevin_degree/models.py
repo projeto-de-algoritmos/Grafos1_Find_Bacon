8254cd395004356ceddafb1ac59a312e3b763cc4
joaovitor3 <joaovytor0@gmail.com>
2021-08-06 01:08:42 -0300

    Update models without search model

--- find_kevin_bacon/kevin_degree/models.py
+++ find_kevin_bacon/kevin_degree/models.py
@@ -1,8 +1,4 @@
 from django.db import models
 
-# Create your models here.
 class Graph(models.Model):
     json_graph = models.JSONField()
-
-class Search(models.Model):
-    search_text = models.CharField(max_length=250)
