ad41e29af54cf9d31449c0cd25cd296d37fdbbfa
joaovitor3 <joaovytor0@gmail.com>
2021-08-07 00:27:13 -0300

    Update DFS code and function to parse integer

--- find_kevin_bacon/kevin_degree/management/commands/build_graph.py
+++ find_kevin_bacon/kevin_degree/management/commands/build_graph.py
@@ -1,16 +1,19 @@
+import json
 from kevin_degree.client import WikipediaClient
 from kevin_degree.graph import KevinBaconGraph
 from django.core.management.base import BaseCommand
 from kevin_degree.models import Graph
 
+
 class Command(BaseCommand):
     help = 'Build graph from wikipedia page links using Kevin Bacon as starting node'
 
     def add_arguments(self, parser):
-        parser.add_argument("--qty-links", type=int, help="number of links to be searched within a page")
+        parser.add_argument("--qty-links", type=int,
+                            help="number of links to be searched within a page")
 
     def handle(self, *args, **kwargs):
-        qty_links = kwargs["qty-links"] if "qty-links" in kwargs else 2
+        qty_links = kwargs["qty_links"] if "qty_links" in kwargs else 2
 
         wikipedia_client = WikipediaClient(max_links_returned=qty_links)
         kevin_bacon_page_links = wikipedia_client.get_page_links("Kevin Bacon")
@@ -18,13 +21,17 @@ class Command(BaseCommand):
             "Kevin Bacon": kevin_bacon_page_links
         }
         pages_set = set()
-        
-        kevin_bacon_graph = KevinBaconGraph()
-        kevin_bacon_graph.build_graph_from_wikipedia_page(kevin_bacon_page_links, graph, pages_set, wikipedia_client)
-        kevin_bacon_graph.add_nodes_without_connections_to_graph(graph, pages_set)
-        path = kevin_bacon_graph.dfs(graph,'Kevin Bacon', "(I'm Gonna) Love Me Again")
-        # db_graph = Graph.objects.create(json_graph=graph)
-        print(path)
-        # print(graph)
 
-    
+        kevin_bacon_graph = KevinBaconGraph()
+        kevin_bacon_graph.build_graph_from_wikipedia_page(
+            kevin_bacon_page_links, graph, pages_set, wikipedia_client)
+        kevin_bacon_graph.add_nodes_without_connections_to_graph(
+            graph, pages_set)
+        graph_integer = kevin_bacon_graph.parse_graph_to_integer(graph)
+
+        start = 0 # 0 represents Kevin Bacon wikipedia page
+        goal = 3
+        path = kevin_bacon_graph.dfs(graph_integer, start, goal)
+        Graph.objects.create(json_graph=graph)
+
+        print(f'PATH: {path}')
